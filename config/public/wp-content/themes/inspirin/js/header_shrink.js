de48bd7b27eb9843d4884d984de8c7421279f1fc
Diego Ocampo <diego@chattyhive.com>
2014-01-10 17:19:27 +0100

    now changing the animation speed

--- config/public/wp-content/themes/inspirin/js/header_shrink.js
+++ config/public/wp-content/themes/inspirin/js/header_shrink.js
@@ -10,14 +10,14 @@ jQuery(document).ready(function ($) {
 		if ($('body').scrollTop() > 0) {
 		    if ($nav.data('size') == 'big') {
 		        $nav.data('size','small').stop().animate({
-		            margin:'0px'
-		        }, 600);
+		            margin-top:'0px'
+		        }, 300);
 		    }
 		} else {
 		    if ($nav.data('size') == 'small') {
 		        $nav.data('size','big').stop().animate({
-		            margin:'5px'
-		        }, 600);
+		            margin-top:'5px'
+		        }, 300);
 		    }  
 		}
 	});
