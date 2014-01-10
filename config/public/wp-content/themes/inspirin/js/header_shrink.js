c57b0ea7ba27cff8a15ace4e69f0b3f30ccbcb07
Diego Ocampo <diego@chattyhive.com>
2014-01-10 16:38:38 +0100

    pruebas menu arriba

---
+++ config/public/wp-content/themes/inspirin/js/header_shrink.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('.wrapper').data('size','big');
+});
+
+$(window).scroll(function(){
+    var $nav = $('.wrapper');
+    if ($('body').scrollTop() > 0) {
+        if ($nav.data('size') == 'big') {
+            $nav.data('size','small').stop().animate({
+                margin:'0px'
+            }, 600);
+        }
+    } else {
+        if ($nav.data('size') == 'small') {
+            $nav.data('size','big').stop().animate({
+                margin:'5px'
+            }, 600);
+        }  
+    }
+});
