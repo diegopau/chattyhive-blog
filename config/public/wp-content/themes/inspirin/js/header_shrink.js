68a8818f706fcf6102c323601126d6bd9ab06340
Diego Ocampo <diego@chattyhive.com>
2014-01-10 17:13:57 +0100

    pequeño cambio

--- config/public/wp-content/themes/inspirin/js/header_shrink.js
+++ config/public/wp-content/themes/inspirin/js/header_shrink.js
@@ -1,21 +1,25 @@
-jQuery(function($){
-    $('#header').data('size','big');
-});
+jQuery(document).ready(function ($) {
+	
+	$(function(){
+		$('#header').data('size','big');
+	});
 
-jQuery(window).scroll(function($){
-console.log(jQuery(window).scrollTop());
-    var $nav = $('#header');
-    if ($('body').scrollTop() > 0) {
-        if ($nav.data('size') == 'big') {
-            $nav.data('size','small').stop().animate({
-                margin:'0px'
-            }, 600);
-        }
-    } else {
-        if ($nav.data('size') == 'small') {
-            $nav.data('size','big').stop().animate({
-                margin:'5px'
-            }, 600);
-        }  
-    }
+	$(window).scroll(function(){
+	console.log($(window).scrollTop());
+		var $nav = $('#header');
+		if ($('body').scrollTop() > 0) {
+		    if ($nav.data('size') == 'big') {
+		        $nav.data('size','small').stop().animate({
+		            margin:'0px'
+		        }, 600);
+		    }
+		} else {
+		    if ($nav.data('size') == 'small') {
+		        $nav.data('size','big').stop().animate({
+		            margin:'5px'
+		        }, 600);
+		    }  
+		}
+	});
+
 });
