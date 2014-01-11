91e92970a15328f82a460d82ae80724ccde8b479
Diego Ocampo <diego@chattyhive.com>
2014-01-11 17:13:36 +0100

    another aproach for nav bar shrink when resizing window

--- config/public/wp-content/themes/inspirin/js/header_shrink.js
+++ config/public/wp-content/themes/inspirin/js/header_shrink.js
@@ -10,22 +10,18 @@ jQuery(document).ready(function ($) {
 			var $nav = $('.cabecera');
 			if ($('body').scrollTop() > 0) {
 			    if ($nav.data('size') == 'big') {
-			        $nav.data('size','small').stop().animate({
-		    	        marginTop:'0px',
-						marginBottom:'0px'
-		    	    }, 100);
+			        $nav.data('size','small').stop().animate({marginTop:'0px',marginBottom:'0px'}, 100);
 					$('#main').css("margin","130px 0 0");
 					$('.ft-logo img').css("cssText", "max-width: 70% !important");
 					$('.ft-logo').css("margin","8px 0 8px 0");
 					$('.menuarriba').css("font-size","17px");
 					$('.ft-icons a i').css("font-size","20px");
-		    	}else {
+		    	}
+			}
+			else {
 				if ($nav.data('size') == 'small') {
 					$('#main').css("margin","90px 0 0");
-				    $nav.data('size','big').stop().animate({
-				        marginTop:'22px',
-						marginBottom:'25px'
-			        }, 100);
+				    $nav.data('size','big').stop().animate({marginTop:'22px',marginBottom:'25px'}, 100);
 					$('.ft-logo img').css("cssText", "max-width: 100% !important");
 					$('.ft-logo').css("margin","0px 0 0px 0");
 					$('.menuarriba').css("font-size","20px");
