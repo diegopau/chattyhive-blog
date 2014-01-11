7bb2b8fc9480e5d6fa22b3464cf3812da46018a9
Diego Ocampo <diego@chattyhive.com>
2014-01-11 17:07:06 +0100

    another aproach for nav bar shrink when resizing window

--- config/public/wp-content/themes/inspirin/js/header_shrink.js
+++ config/public/wp-content/themes/inspirin/js/header_shrink.js
@@ -4,47 +4,36 @@ jQuery(document).ready(function ($) {
 		$('.cabecera').data('size','big');
 	});
 
-	comprobar();
+	
+	if ($('#header').css("position")=="fixed"){
+		$(window).scroll(function(){
+			var $nav = $('.cabecera');
+			if ($('body').scrollTop() > 0) {
+			    if ($nav.data('size') == 'big') {
+			        $nav.data('size','small').stop().animate({
+		    	        marginTop:'0px',
+						marginBottom:'0px'
+		    	    }, 100);
+					$('#main').css("margin","130px 0 0");
+					$('.ft-logo img').css("cssText", "max-width: 70% !important");
+					$('.ft-logo').css("margin","8px 0 8px 0");
+					$('.menuarriba').css("font-size","17px");
+					$('.ft-icons a i').css("font-size","20px");
+		    	}else {
+				if ($nav.data('size') == 'small') {
+					$('#main').css("margin","90px 0 0");
+				    $nav.data('size','big').stop().animate({
+				        marginTop:'22px',
+						marginBottom:'25px'
+			        }, 100);
+					$('.ft-logo img').css("cssText", "max-width: 100% !important");
+					$('.ft-logo').css("margin","0px 0 0px 0");
+					$('.menuarriba').css("font-size","20px");
+					$('.ft-icons a i').css("font-size","24px");
+			    }  
+			}
+		});
 
-	function comprobar()
-	{	
-		var windowWidth = $(window).innerWidth();
-
-		if (windowWidth > 767) 
-		{
-			$(window).scroll(function(){
-				var $nav = $('.cabecera');
-				if ($('body').scrollTop() > 0) {
-				    if ($nav.data('size') == 'big') {
-				        $nav.data('size','small').stop().animate({
-			    	        marginTop:'0px',
-							marginBottom:'0px'
-			    	    }, 100);
-						$('#main').css("margin","130px 0 0");
-						$('.ft-logo img').css("cssText", "max-width: 70% !important");
-						$('.ft-logo').css("margin","8px 0 8px 0");
-						$('.menuarriba').css("font-size","17px");
-						$('.ft-icons a i').css("font-size","20px");
-			    	}
-				} else {
-				    if ($nav.data('size') == 'small') {
-						$('#main').css("margin","90px 0 0");
-				        $nav.data('size','big').stop().animate({
-				            marginTop:'22px',
-							marginBottom:'25px'
-				        }, 100);
-						$('.ft-logo img').css("cssText", "max-width: 100% !important");
-						$('.ft-logo').css("margin","0px 0 0px 0");
-						$('.menuarriba').css("font-size","20px");
-						$('.ft-icons a i').css("font-size","24px");
-				    }  
-				}
-			});
-
-		}
-
-		$(window).resize(function () {
-			comprobar()
-  		});
 	}
+
 });
