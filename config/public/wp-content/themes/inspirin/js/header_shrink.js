5652706164dad7728e9eff7bf33494b2643744c0
Diego Ocampo <diego@chattyhive.com>
2014-01-17 18:09:09 +0100

    testing header changes

--- config/public/wp-content/themes/inspirin/js/header_shrink.js
+++ config/public/wp-content/themes/inspirin/js/header_shrink.js
@@ -3,30 +3,13 @@ jQuery(document).ready(function ($) {
 	$(function(){
 		$('.cabecera').data('size','big');
 	});
-
+	
+	var $nav = $('.cabecera');
+	
 	$(window).resize(function() {
-		var $nav = $('.cabecera');
+		
 		if ($('#header').css("position")=="fixed"){
-			$(window).scroll(function(){
-				if ($('body').scrollTop() > 70) {
-					if ($nav.data('size') == 'big') {
-						$nav.data('size','small').stop().animate({marginTop:'0px',marginBottom:'0px'}, 200);
-						$('#main').css("margin","130px 0 0");
-						$('.ft-logo img').css("cssText", "max-width: 70% !important");
-						$('.ft-logo').css("margin","8px 0 8px 0");
-						$('.ft-icons a i').css("font-size","20px");
-					}
-				}
-				else {
-					if ($nav.data('size') == 'small') {
-						$('#main').css("margin","90px 0 0");
-						$nav.data('size','big').stop().animate({marginTop:'22px',marginBottom:'25px'}, 50);
-						$('.ft-logo img').css("cssText", "max-width: 100% !important");
-						$('.ft-logo').css("margin","0px 0 0px 0");
-						$('.ft-icons a i').css("font-size","24px");
-					}  
-				}
-			});
+		  
 		}
 		else{
 			$('#main').css("margin","0px 0 0");
@@ -35,9 +18,31 @@ jQuery(document).ready(function ($) {
 			$('.ft-logo').css("margin","0px 0 0px 0");
 			$('.ft-icons a i').css("font-size","24px");
 		}
-		      
+	}
+	
 
-		
+	$(window).scroll(function(){
+		if f ($('#header').css("position")=="fixed"){
+			if ($('body').scrollTop() > 70){
+				if ($nav.data('size') == 'big') {
+					$nav.data('size','small').stop().animate({marginTop:'0px',marginBottom:'0px'}, 50);
+					$('#main').css("margin","130px 0 0");
+					$('.ft-logo img').css("cssText", "max-width: 70% !important");
+					$('.ft-logo').css("margin","8px 0 8px 0");
+					$('.ft-icons a i').css("font-size","20px");
+				}
+			}
+			else {
+				if ($nav.data('size') == 'small') {
+					$('#main').css("margin","90px 0 0");
+					$nav.data('size','big').stop().animate({marginTop:'22px',marginBottom:'25px'}, 200);
+					$('.ft-logo img').css("cssText", "max-width: 100% !important");
+					$('.ft-logo').css("margin","0px 0 0px 0");
+					$('.ft-icons a i').css("font-size","24px");
+				}
+			}
+		}
 	});
+	      
 
     });
