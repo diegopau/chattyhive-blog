88e0d063b7d3e01667682e90f10f5a1751cd0955
Diego Ocampo <diego@chattyhive.com>
2014-01-18 23:38:18 +0100

    few more css fixes

--- config/public/wp-content/themes/inspirin/js/header_shrink.js
+++ config/public/wp-content/themes/inspirin/js/header_shrink.js
@@ -16,6 +16,14 @@ jQuery(document).ready(function ($) {
 			$('.ft-icons a i').css("font-size","24px");
 			$('.menuarriba').css("margin-right","175px");
 		}
+		else{
+			$nav.data('size','small').stop().animate({marginTop:'0px',marginBottom:'0px'}, 200);
+			$('#main').css("margin","140px 0 0");
+			$('.ft-logo img').css("cssText", "max-width: 70% !important");
+			$('.ft-logo').css("margin","8px 0 8px 0");
+			$('.ft-icons a i').css("font-size","20px");
+			$('.menuarriba').css("margin-right","200px");
+		}
 	});
 	
 
@@ -24,7 +32,7 @@ jQuery(document).ready(function ($) {
 			if (Math.max(window.pageYOffset || document.documentElement.scrollTop || $('body').scrollTop || 0) > 70){
 				if ($nav.data('size') == 'big') {
 					$nav.data('size','small').stop().animate({marginTop:'0px',marginBottom:'0px'}, 200);
-					$('#main').css("margin","130px 0 0");
+					$('#main').css("margin","140px 0 0");
 					$('.ft-logo img').css("cssText", "max-width: 70% !important");
 					$('.ft-logo').css("margin","8px 0 8px 0");
 					$('.ft-icons a i').css("font-size","20px");
