0ab286c4efce07d1c5951363e7296bc439de60f5
Diego Ocampo <diego@chattyhive.com>
2014-01-26 14:05:55 +0100

    fixes and image preload for isotipo

--- config/public/wp-content/themes/inspirin/footer.php
+++ config/public/wp-content/themes/inspirin/footer.php
@@ -4,9 +4,13 @@
     <div class="w-container">
       <a class="footer-link" href="http://join.chattyhive.com">chattyhive &nbsp;&nbsp;&#x2022;</a><a class="footer-link" href="http://join.chattyhive.com/about">&#x00bf;Qui&eacute;nes somos?</a><a class="footer-link" href="http://join.chattyhive.com/about#escribenos">&#x2022; &nbsp;&nbsp;Escr&iacute;benos</a>
     </div>
-    <a class="w-inline-block logo_foot" href="#slogan-section"></a>
+    <a class="w-inline-block logo_foot" href="http://blog.chattyhive.com"></a>
   </div>
 </div>
+
+  <div id="preload">
+   <img src="{{images/isotipo_sombreado_sin_gradiente_transparente-02.png" width="1" height="1" alt="isotipo amarillo" />
+  </div>
 
 <aside class="ft-backtop">
 	<div class="back-top">
