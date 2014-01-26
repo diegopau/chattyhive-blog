233785eac8b147a98e0dc108681dffa644bff754
Diego Ocampo <diego@chattyhive.com>
2014-01-26 12:48:35 +0100

    changing footer

--- config/public/wp-content/themes/inspirin/footer.php
+++ config/public/wp-content/themes/inspirin/footer.php
@@ -1,41 +1,11 @@
 <?php global $smof_data; ?>
-<footer id="footer">
-	<div class="ft-rdposts clearfix">
-		<div class="wrapper clearfix">
-		<div class="ft-rdtitle">
-			<i class="icon-star-empty"></i>
-			<p><?php _e('Top Article', 'ft'); ?></p>
-			<span><?php _e('Don\'t Miss', 'ft'); ?></span>
-		</div>
-		<div class="ft-rdpost">
-			<?php query_posts('showposts=1&orderby=rand&ignore_sticky_posts=1'); the_post(); ?>
-			<p><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title_limit( 65, '...'); ?></a></p>
-			<span><?php _e('On', 'ft'); ?> <time class="updated meta-button" datetime="<?php the_time('F j, Y'); ?>" pubdate> <?php the_time('F j, Y'); ?></time> <?php _e('With', 'ft'); ?> <?php comments_popup_link(__('0 Comments', 'ft'), __('1 Comment', 'ft'), '% '.__('Comments', 'ft')); ?></span>
-			<?php wp_reset_query(); ?> 
-		</div>
-		</div>
-	</div>
-	<div class="wrapper clearfix">
-		<div class="ft-widgets clearfix">
-			<?php dynamic_sidebar("Footer Widgets"); ?>
-		</div>
-	</div>
-	<div class="ft-ftmenu clearfix">
-		<div class="wrapper">
-			<?php if ( has_nav_menu( 'footer_navigation' ) ): ?>
-			<?php wp_nav_menu( array('theme_location' => 'footer_navigation','container' => false,'menu_class' => 'footermenu','echo' => true,'before' => '','after' => '','link_before' => '','link_after' => '','depth' => 3,'items_wrap' => '<ul class="footermenu">%3$s</ul>')); ?>
-			<?php else: ?>
-			<p class="ft-ftnomenus">Please Setup Your Menus : Dashboard -> Appearance -> Menus</p>
-			<?php endif; ?>
-		</div>
-	</div>
-	<div class="ft-copyright clearfix">
-		<div class="wrapper">
-			<div class="ft-cptext"><?php if(!empty($smof_data['ft_footer_text'])): ?><?php echo $smof_data['ft_footer_text']; ?><?php endif; ?></div>
-		</div>
-	</div>
-</footer>
 
+  <div class="section grey footer">
+    <div class="w-container">
+      <a class="footer-link" href="#slogan-section">chattyhive &nbsp;&nbsp;&#x2022;</a><a class="footer-link" href="about">&#x00bf;Qui&eacute;nes somos?</a><a class="footer-link" href="about#escribenos">&#x2022; &nbsp;&nbsp;Escr&iacute;benos</a>
+    </div>
+    <a class="w-inline-block logo_foot" href="#slogan-section"></a>
+  </div>
 </div>
 
 <aside class="ft-backtop">
