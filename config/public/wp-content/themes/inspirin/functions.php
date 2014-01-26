ad09b0fa8dac29c429ee3736dd8bea7725cfa1a5
Diego Ocampo <diego@chattyhive.com>
2014-01-26 14:48:59 +0100

    Prevent scroll when clicking in Continue Reading

--- config/public/wp-content/themes/inspirin/functions.php
+++ config/public/wp-content/themes/inspirin/functions.php
@@ -255,3 +255,13 @@ function catch_that_image() {
   }
   return $first_img;
 }
+
+/*-----------------------------------------------------------------------------------*/
+/* Prevent scroll when clicking in Continue Reading
+/*-----------------------------------------------------------------------------------*/
+
+function remove_more_link_scroll( $link ) {
+	$link = preg_replace( '|#more-[0-9]+|', '', $link );
+	return $link;
+}
+add_filter( 'the_content_more_link', 'remove_more_link_scroll' );
