b2d8d5f202f551144ce17c1ec731e99c479e8e84
Diego Ocampo <diego@chattyhive.com>
2014-07-11 23:57:05 +0200

    adding comment number for each post in index page

--- config/public/wp-content/themes/inspirin/lib/theme-loops/loop.php
+++ config/public/wp-content/themes/inspirin/lib/theme-loops/loop.php
@@ -7,6 +7,7 @@
 		<header><h2 class="ft-ptitle entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2></header>
 		<div class="ft-bmeta"><span class="ft-author vcard author"><span class="fn"><?php the_author_posts_link(); ?></span></span> - <span class="ft-time"><time class="updated" datetime="<?php the_time('F, jS Y'); ?>"><?php the_time('F j, Y'); ?></time></span> - <span class="ft-categories"><?php the_category(', '); ?></span></div>
 		<section class="ft-entry"><?php the_content(__('Continue Reading','ft')); ?></section>
+		<div><?php comments_number( 'no comments', 'one comment', '% comments' ); ?>.</div>
 	</div>
 </article>
 
